068425bb07ac268b4bd5609eba7fc55b70d3dcfa
Rishit Shah <aryanrishit1@gmail.com>
2025-08-27 22:47:38 +0530

    Commit 1 [26:03]: Setup the basic file structure of the project and got the required background info to make it.

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/api/notes", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(5001, () => {
+    console.log("Server started on port 5001")
+});
