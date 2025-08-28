a6185b7ca8e1f65976273f59768dfcd363fe4d14
Rishit Shah <aryanrishit1@gmail.com>
2025-08-28 19:17:23 +0530

    Commit 4 [1:03:56]: Connected MongoDB with the codebase and made a model for the notes.

---
+++ backend/src/models/Note.js
@@ -0,0 +1,22 @@
+// for models, we capitalize the first letter of the file name and keep the name as a singular noun
+
+// Step 1: Create Schema
+// Step 2: Create a model based on that schema
+
+const noteSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },    
+    },
+    {timestamps: true} // createdAt, updatedAt automatically provided by mongoose
+);
+
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note;
