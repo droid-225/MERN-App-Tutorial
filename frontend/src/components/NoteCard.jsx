1136fb737c40a6c7f1915baf37cd8a1be87b7d72
Rishit Shah <aryanrishit1@gmail.com>
2025-08-30 18:14:24 +0530

    Commit 10 [2:57:24]: Implemented the delete functionality and a UI case if there are no notes to be shown.

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -2,8 +2,25 @@ import { PenSquareIcon, Trash2Icon } from 'lucide-react';
 import React from 'react';
 import {Link} from 'react-router';
 import {formatDate} from '../lib/utils'
+import api from '../lib/axios';
+import toast from 'react-hot-toast';
 
-const NoteCard = ({note}) => {
+const NoteCard = ({note, setNotes}) => {
+  const handleDelete = async (e, id) => {
+    e.preventDefault(); // get rid of Link nav behaviour
+
+    if(!window.confirm("Are you sure you want to delte this note?")) return;
+    
+    try {
+        api.delete(`/notes/${id}`);
+        setNotes((prev) => prev.filter(note => note._id !== id)); // removes deleted note from array
+        toast.success("Note deleted successfully!");
+    } catch(error) {
+        console.log("Error in handleDelete", error);
+        toast.error("Failed to delete note");
+    }  
+  };
+
   return <Link to={`/note/${note._id}`} 
     className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D]"
     >
@@ -14,7 +31,7 @@ const NoteCard = ({note}) => {
                 <span className="text-sm text-base-content/60">{formatDate(new Date(note.createdAt))}</span>
                 <div className="flex items-center gap-1">
                     <PenSquareIcon className="size-4"/>
-                    <button className="btn btn-ghost btn-xs text-error">
+                    <button className="btn btn-ghost btn-xs text-error" onClick={(e) => handleDelete(e, note._id)}>
                         <Trash2Icon className="size-4"/>
                     </button>
                 </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,8 +3,8 @@ import Navbar from '../components/Navbar';
 import RateLimitUI from '../components/RateLimitUI';
 import NoteCard from '../components/NoteCard';
 import api from "../lib/axios.js";
-import axios from 'axios';
 import toast from "react-hot-toast";
+import NotesNotFound from '../components/NotesNotFound.jsx';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -44,10 +44,12 @@ const HomePage = () => {
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading Notes...</div>}
 
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map(note => (
-              <NoteCard key={note.id} note={note}/>
+              <NoteCard key={note._id} note={note} setNotes={setNotes}/>
             ))}
           </div>
         )}
