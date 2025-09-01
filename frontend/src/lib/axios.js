520fe5568e861d8ffab98c4503b808fd51505c47
Rishit Shah <aryanrishit1@gmail.com>
2025-09-01 10:41:04 +0530

    Commit 16: Removed new env variables, turns out I wrote evn instead of env in the axios lib.

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // in production, there is no localhost so we need to make base url dynamic
-const BASE_URL = import.meta.evn.MODE === "development" ? "http://localhost:5001/api" : "/api"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
     baseURL: BASE_URL,
 });
